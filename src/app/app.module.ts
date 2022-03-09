801113e82984228d7d705d5d9e12b355c4e8e923
siyun_fu <siyun.fu@tpisoftware.com>
2022-03-09 11:03:08 +0800

    delete app initializer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
-import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { appInitializer } from './_helpers/app.initializer';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule],
-  providers: [{ provide: APP_INITIALIZER, useFactory: appInitializer }],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
